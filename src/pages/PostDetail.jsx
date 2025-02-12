c1e674427d5cc7d7e5cfea9b9e7a66d1e4b01115
jinew <jiyunk@naver.com>
2025-02-12 18:16:56 +0900

    🍻 Test : supabase 출력 테스트

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,4 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import supabase from '../shared/supabaseClient';
+
 const PostDetail = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from('TASTY_DB').select('*');
+      if (error) {
+        console.log('error=> ', error);
+      } else {
+        console.log('data=> ', data);
+        setUsers(data);
+      }
+    };
+    fetchData();
+  }, []);
+
   return <div>PostDetail</div>;
 };
 

9f0030ccf62c5704ff81e79aac803d2b09833df4
jinew <jiyunk@naver.com>
2025-02-12 21:45:09 +0900

    🩹 Fix : 파일명 변경으로 인한 라우팅 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
-import MemberLogin from '../pages/MemberLogin';
-import MemberJoin from '../pages/MemberJoin';
 import MyPage from '../pages/MyPage';
 import PostWrite from '../pages/PostWrite';
 import PostDetail from '../pages/PostDetail';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<MemberLogin />}></Route>
-        <Route path="/join" element={<MemberJoin />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/post-write" element={<PostWrite />}></Route>
         <Route path="/post-detail" element={<PostDetail />}></Route>

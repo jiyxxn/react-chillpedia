21f62cde0373efe8b222cb99533cd6edb94136f0
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-13 23:46:58 +0900

    🎨 Styling : Login CSS 모양새 만들기

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,28 +23,34 @@ const Login = () => {
   return (
     <LoginContainer>
       <LoginTitle>로그인</LoginTitle>
-      <LoginBox onSubmit={handleLogin}>
-        <AuthContainer>
-          <span>아이디(이메일)</span>
-          <span>비밀번호</span>
-        </AuthContainer>
-        <AuthInput>
-          <input
-            type="email"
-            value={email}
-            placeholder="이메일"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            value={password}
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </AuthInput>
-        <button type="submit">로그인</button>
-        <button>회원가입</button>
-      </LoginBox>
+      <div>
+        <LoginBox onSubmit={handleLogin}>
+          <AuthContainer>
+            <AuthSpan>아이디(이메일)</AuthSpan>
+            <Divider />
+            <AuthSpan>비밀번호</AuthSpan>
+          </AuthContainer>
+          <AuthInput>
+            <LoginInput
+              type="email"
+              value={email}
+              placeholder="이메일"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <LoginInput
+              type="password"
+              value={password}
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </AuthInput>
+        </LoginBox>
+
+        <LoginButtonGroup>
+          <LoginButton type="submit">로그인</LoginButton>
+          <SignUpButton>회원가입</SignUpButton>
+        </LoginButtonGroup>
+      </div>
     </LoginContainer>
   );
 };
@@ -60,12 +66,14 @@ const LoginContainer = styled.div`
 const LoginBox = styled.form`
   width: 980px;
   height: 380px;
-  border: 1px solid black;
+  padding: 70px;
+  border: 1px solid #66666e;
   border-radius: 20px;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   flex-direction: row;
   align-items: center;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
 `;
 const LoginTitle = styled.h2`
   font-size: 25px;
@@ -76,14 +84,74 @@ const LoginTitle = styled.h2`
 const AuthContainer = styled.div`
   display: flex;
   flex-direction: column;
-  font-size: 18px;
-  font-weight: bold;
+  justify-content: center;
+  gap: 30px;
+  background-color: #efe1c6;
+  width: 300px;
+  height: 166px;
+  border: none;
+  border-radius: 30px;
+  gap: 30px;
+  text-align: center;
+  align-items: center;
+  text-align: center;
 `;
 
 const AuthInput = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 33px;
+`;
+
+const AuthSpan = styled.span`
+  font-size: 18px;
   font-weight: bold;
 `;
 
+const Divider = styled.hr`
+  width: 100%;
+  border: none;
+  border-top: 2px solid #66666e;
+  margin: 0.5px 0;
+`;
+
+const LoginInput = styled.input`
+  width: 500px;
+  height: 50px;
+  border: 1px solid black;
+  border-radius: 50px;
+  text-align: center;
+`;
+
+const LoginButtonGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 25px;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const LoginButton = styled.button`
+  width: 48%;
+  height: 80px;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+  border: 1px solid #66666e;
+  border-radius: 20px;
+  background-color: #efe1c6;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+`;
+
+const SignUpButton = styled.button`
+  width: 48%;
+  height: 80px;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+  border: 1px solid #66666e;
+  border-radius: 20px;
+  background-color: #dcdcdc;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+`;
 export default Login;

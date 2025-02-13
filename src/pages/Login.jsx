d2d83e8bec609f3f42090330a1895786ce8cc583
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-13 22:02:39 +0900

    ✨ Update : 로그인 시 Home으로 이동 아직 검증은 구현 안됫음

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
+import supabase from '../shared/supabaseClient';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await supabase.auth.signInWithPassword({ email, password });
+      alert('로그인이 완료되었습니다. 홈 페이지로 이동합니다.');
+      navigate('/');
+    } catch (error) {
+      console.error('로그인 오류:', error);
+    }
+  };
+
   return (
     <LoginContainer>
-    <LoginTitle>로그인</LoginTitle>
-    <LoginBox>
-    <AuthContainer>
-      <span>아이디(이메일)</span>
-      <span>비밀번호</span>
-    </AuthContainer>
-    <AuthInput>
-      <input type="email" placeholder='이메일' />
-      <input type="password" placeholder='password' />
-    </AuthInput>
-    </LoginBox>
+      <LoginTitle>로그인</LoginTitle>
+      <LoginBox onSubmit={handleLogin}>
+        <AuthContainer>
+          <span>아이디(이메일)</span>
+          <span>비밀번호</span>
+        </AuthContainer>
+        <AuthInput>
+          <input
+            type="email"
+            value={email}
+            placeholder="이메일"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            value={password}
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </AuthInput>
+        <button type="submit">로그인</button>
+        <button>회원가입</button>
+      </LoginBox>
     </LoginContainer>
   );
 };
@@ -25,7 +55,7 @@ const LoginContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-`
+`;
 
 const LoginBox = styled.form`
   width: 980px;
@@ -36,24 +66,24 @@ const LoginBox = styled.form`
   justify-content: center;
   flex-direction: row;
   align-items: center;
-`
+`;
 const LoginTitle = styled.h2`
   font-size: 25px;
   margin-bottom: 35px;
   font-weight: bold;
-`
+`;
 
 const AuthContainer = styled.div`
   display: flex;
   flex-direction: column;
   font-size: 18px;
   font-weight: bold;
-`
+`;
 
 const AuthInput = styled.div`
   display: flex;
   flex-direction: column;
   font-weight: bold;
-`
+`;
 
 export default Login;

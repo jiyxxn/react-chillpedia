3739b62270052eae7d9f8bc4e412ccc025ee24d1
jinew <jiyunk@naver.com>
2025-02-13 14:19:47 +0900

    🎉 Add : 전역 상태 관리 추가

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState, createContext } from 'react';
+import supabase from '../shared/supabaseClient';
+
+const UserLoginContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session) {
+        setIsLogin(true);
+        setUser(session.user);
+      } else {
+        setIsLogin(false);
+        setUser(null);
+      }
+    });
+  }, []);
+
+  return (
+    <UserLoginContext.Provider value={{ isLogin }}>
+      {children}
+    </UserLoginContext.Provider>
+  );
+};
+
+export default AuthProvider;

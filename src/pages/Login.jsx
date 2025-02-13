d1e1bc399cfb5285988581319c0fdccad546587b
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-13 20:00:44 +0900

    🎨 Style : Login 컴포넌트 CSS 이미지 설정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <LoginContainer>
+    <LoginTitle>로그인</LoginTitle>
+    <LoginBox>
+    <AuthContainer>
+      <span>아이디(이메일)</span>
+      <span>비밀번호</span>
+    </AuthContainer>
+    <AuthInput>
+      <input type="email" placeholder='이메일' />
+      <input type="password" placeholder='password' />
+    </AuthInput>
+    </LoginBox>
+    </LoginContainer>
+  );
 };
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const LoginBox = styled.form`
+  width: 980px;
+  height: 380px;
+  border: 1px solid black;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+`
+const LoginTitle = styled.h2`
+  font-size: 25px;
+  margin-bottom: 35px;
+  font-weight: bold;
+`
+
+const AuthContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const AuthInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+`
 
 export default Login;

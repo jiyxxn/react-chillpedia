9344245aa8075c65efb8d070031c76629e044bfd
sohxxny <sohxxny@gmail.com>
2025-02-14 11:08:06 +0900

    ✨ Update : 회원가입 시 닉네임 추가
    - profiles 테이블의 uuid 설정을 unique, not nullable로 변경
    - profiles 테이블의 uuid가 auth.users.id를 참조하도록 설정
    - auth -> profiles 트리거 작성
    - 회원가입 시 profiles에 닉네임, uuid 값 설정한 row 생성
    
    resolve #2

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-import { useState } from "react";
-import supabase from "../shared/supabaseClient";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useState } from 'react';
+import supabase from '../shared/supabaseClient';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nickname, setNickname] = useState('');
   const navigate = useNavigate();
 
   const handleSingup = async (e) => {
     e.preventDefault();
 
     try {
-      await supabase.auth.signUp({
+      const { error } = await supabase.auth.signUp({
         email,
         password,
+        options: {
+          data: {
+            nickname,
+          },
+        },
       });
-      alert("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
-      navigate("/login");
+
+      if (error) {
+        console.log(error.name);
+        switch (error.code) {
+          case 'user_already_exists':
+            alert('이미 존재하는 이메일입니다.');
+            return;
+          case 'weak_password':
+            alert('보안에 취약한 비밀번호입니다.');
+            return;
+          default:
+            alert(`회원가입 에러 : ${error.code}`);
+        }
+      } else {
+        alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');
+        navigate('/login');
+      }
     } catch (error) {
-      alert("회원가입 오류");
-      console.error("회원가입 오류:", error.message);
+      alert(`회원가입 에러 : ${error.message}`);
     }
   };
 
@@ -40,6 +60,13 @@ const Signup = () => {
           placeholder="비밀번호"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="닉네임"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
           required
         />
         <button type="submit">회원가입</button>
